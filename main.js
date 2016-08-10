e8066188739db0bd316227f1dd68135374dac88d
duhdanube <Danny.B.Lim@gmail.com>
2016-08-10 00:37:38 -0700

    CRUD

--- main.js
+++ main.js
@@ -49,33 +49,52 @@ const NewMessageForm = React.createClass({
   }
 });
 
-const MessageDeleteButton = React.createClass({
-  deleteMessage: function() {
-    console.log(this.props.deleteId);
-    /*for (var i = 0; i < this.props.messages; i++) {
-      console.log(i);
-      if (this.prop.messages[i].id === messageId) {
-        index = i;
-      }
-    }*/
-    /*this.setState({
-      messages: this.prop.messages.splice(index, 1);
-    })*/
+const EditForm = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ""
+    }
+  },
+  onInputChange: function(event) {
+    this.setState({text: event.target.value})
+  },
+  confirmChange: function() {
+    this.props.editUpdate(this.props.editId, this.state.text);
+    this.props.formVisible();
   },
   render: function() {
     return (
-      <button className="btn btn-default" onClick={this.deleteMessage}>Delete</button>
+      <div hidden={this.props.hidden}>
+        <input type="text" value={this.state.text} onChange={this.onInputChange}/>
+        <button onClick={this.confirmChange}>Confirm</button>
+      </div>
     )
   }
 })
 
-const MessageUpdateButton = React.createClass({
-  updateMessage: function() {
-    console.log(this.props.updateId);
+const MessageLi = React.createClass({
+  getInitialState: function() {
+    return {
+      visible: "hidden"
+    }
+  },
+  deleteMessage: function() {
+    this.props.deleteMsg(this.props.msgId);
+  },
+  setFormHidden: function() {
+    this.setState({visible: "hidden"});
+  },
+  updateButtonClick: function() {
+    this.setState({visible: ""});
   },
   render: function() {
     return (
-      <button className="btn btn-primary" onClick={this.updateMessage}>Update</button>
+      <li>
+      {this.props.value}
+      <EditForm hidden={this.state.visible} editId={this.props.msgId} formVisible={this.setFormHidden} editUpdate={this.props.updateMsg}/>
+      <button className="btn btn-primary" onClick={this.updateButtonClick}>Update</button>
+      <button className="btn btn-default" onClick={this.deleteMessage}>Delete</button>
+      </li>
     )
   }
 })
@@ -84,11 +103,7 @@ const MessageList = React.createClass({
   render: function() {
     let messages = this.props.messages.map(message => {
       return (
-        <li key={message.id}>
-          {message.text}
-          <MessageDeleteButton deleteId={message.id} />
-          <MessageUpdateButton updateId={message.id} />
-        </li>
+        <MessageLi key={message.id} value={message.text} deleteMsg={this.props.delete} updateMsg={this.props.update} msgId={message.id}/>
       )
     });
 
@@ -104,7 +119,7 @@ const MessageBoard = React.createClass({
   getInitialState: function() {
     return {
       messages: []
-  };
+    }
   },
   addMessage: function(text) { //takes message text as argument
     //need to create message object
@@ -118,14 +133,32 @@ const MessageBoard = React.createClass({
     this.setState({
       messages: this.state.messages.concat(message)
     })
-
+  },
+  deleteMessage: function(msgId) {
+    let index = 0;
+    let newArr = this.state.messages.filter(message => {
+      return message.id !== msgId;
+    });
+    this.setState({messages: newArr});
+  },
+  updateMessage: function(msgId, msgText) {
+    let updMessage = {
+      text: msgText,
+      id: msgId
+    };
+    for (let i = 0; i < this.state.messages.length; i++) {
+      if (this.state.messages[i].id === msgId) {
+        this.state.messages[i] = updMessage;
+      }
+    }
+    this.setState({messages: this.state.messages});
   },
   render: function() {
     return (
       <div>
         <h1>MessageBoard</h1>
         <NewMessageForm addMessage={this.addMessage} />
-        <MessageList messages={this.state.messages}/>
+        <MessageList messages={this.state.messages} delete={this.deleteMessage} update={this.updateMessage}/>
       </div>
     )
   }
